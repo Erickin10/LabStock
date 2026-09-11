432db0758756d6c12635efc6752e9c9c2cc55600
erickbarreto <erickfsbarreto@gmail.com>
2026-09-10 21:13:16 -0300

    chore: initial project setup - Spring Boot 4.1.1 with LabStock structure

---
+++ src/main/java/com/integrador/labstock/LabStockApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.labstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integrador/labstock/LabStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.labstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
