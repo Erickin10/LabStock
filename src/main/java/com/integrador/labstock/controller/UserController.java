623250a8fdb176934c8728e8d6a81559874bac68
Vinnyda23 <vinnycontateme@gmail.com>
2026-09-15 16:54:32 -0300

    Feat/Flyway_teste

--- src/main/java/com/integrador/labstock/controller/UserController.java
+++ src/main/java/com/integrador/labstock/controller/UserController.java
@@ -8,14 +8,7 @@ import com.integrador.labstock.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +18,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<UserResponse>>> listAll (@RequestParam(required = false) String search) {
